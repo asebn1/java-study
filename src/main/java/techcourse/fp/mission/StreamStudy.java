f698f6e90c6b9f05cb65d1921dd878b2c7ae1d5b
asebn1 <asebn1@khu.ac.kr>
2022-03-25 12:15:57 +0900

    refactor: stream 테스트에 맞게 구현

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -6,7 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamStudy {
 
@@ -15,34 +19,26 @@ public class StreamStudy {
             .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream()
+                .map(x -> x*2)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream().mapToInt(Integer::intValue).sum();
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToInt(number -> number * 2)
+                .sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -52,6 +48,14 @@ public class StreamStudy {
 
         System.out.println(words);
         System.out.println(words.size());
-        // TODO 이 부분에 구현한다.
+
+        List<String> top100Words = words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .map(word -> word.toLowerCase())
+                .limit(100)
+                .collect(Collectors.toList());
+        System.out.println(top100Words);
     }
 }
